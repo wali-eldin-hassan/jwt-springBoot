47e4adcbf1e9a2b4b15c7748a48a3be344bb80ce
walieldin.hassan <creaspo6@gmail.com>
2023-03-12 10:22:11 +0200

    certs + public and private key
    openssl genrsa -out keypair.pem 2048
     openssl rsa -in keypair.pem -pubout -out public.pem

--- src/main/java/wali/hassan/jwtmicroServics/config/SecurityConfig.java
+++ src/main/java/wali/hassan/jwtmicroServics/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package wali.hassan.jwtmicroServics.config;
 
+import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -28,6 +30,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         return  http.csrf(csrf->csrf.disable())
                 .authorizeRequests(auth ->auth.anyRequest().authenticated())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt )
                 .sessionManagement(session ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(Customizer.withDefaults())
                 .build();
--- src/main/java/wali/hassan/jwtmicroServics/controller/HomeController.java
+++ src/main/java/wali/hassan/jwtmicroServics/controller/HomeController.java
@@ -3,12 +3,14 @@ package wali.hassan.jwtmicroServics.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 public class HomeController {
     @GetMapping("/")
-    public  String Home()
+    public  String Home(Principal principal)
     {
-        return "Hi There";
+        return "Hi"+ " "  +  principal.getName();
     }
 
 }
